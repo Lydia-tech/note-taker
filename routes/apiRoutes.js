5a03bd6bc1fea8423acbcc18e50334103fedb53c
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2020-12-21 17:42:48 -0600

    current changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,25 +4,33 @@ const data = require("../db/db.json");
 const idGeneration = require('crypto').randomBytes(64).toString('hex');
 
 console.log(`your cryptographically generated 122 character value is ${idGeneration}`)
-
+console.log(data.length)
+const databaseArry = [...data]
+console.log(databaseArry)
 router.get('/notes', (_req, res) => {
-    data.getNotes().then((notes) => {
-        return res.json(notes)
-        // typeof NodeError
-    }).catch((err)) => {res.send(err.code || err.message || err.status).json(err)
+    res.json(data)
+    // data.getNotes().then((notes) => {
+    //     return res.json(notes)
+    //     // typeof NodeError
+    // }).catch((err) => {res.send(err.code || err.message || err.status).json(err)
 })
 // err.code || err.message || err.status).json(err.message || err.code || err.status).json(err.message !==null ? err.message.toString() : err.status !==null  ? err.status :|| err.code || err.status)
 router.post('/notes', (req, res) => {
-    data.addNote(req.body).then((note) => {res.json(note).catch((err) => {res.send(err.code || err.status || err.message).json(err)})})
+console.log(req.body.title)   
+console.log(req.body.text) 
+databaseArry.push (req.body)
+console.log(databaseArry)
+ //   data.addNote(req.body).then((note) => {res.json(note).catch((err) => {res.send(err.code || err.status || err.message).json(err)})})
+//   })
 })
-
 router.delete('/notes/:id', (req, res) => {
     try {
     data
       .removeNote(req.params.id)
       .then(() => res.json({ ok: true }))
       .catch((err) => res.send(err.message).json(err));
-    } catch (err) { return err.status(500).json(err)}
+    } catch (err) { 
+       return err.status(500).json(err)}
     })
 
   module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
