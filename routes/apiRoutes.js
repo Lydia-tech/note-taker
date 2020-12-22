ab612d37d391508a70e459f267a98dee66b6df4b
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2020-12-21 20:44:19 -0600

    added save note function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,6 +15,17 @@ router.get('/notes', (_req, res) => {
     // }).catch((err) => {res.send(err.code || err.message || err.status).json(err)
 })
 // err.code || err.message || err.status).json(err.message || err.code || err.status).json(err.message !==null ? err.message.toString() : err.status !==null  ? err.status :|| err.code || err.status)
+
+router.get('')
+// A function for saving a note to the db
+let saveNote = function(note) {
+    return $.ajax({
+      url: "/api/notes",
+      data: note,
+      method: "POST"
+    });
+  };
+
 router.post('/notes', (req, res) => {
 console.log(req.body.title)   
 console.log(req.body.text) 
