2e9412a4021bd3f5e96b410642622b22a5da6f9c
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-01-02 19:19:40 -0600

    updated port in js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
